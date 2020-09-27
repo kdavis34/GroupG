e8ad3c45e48e1382efaf30110336f0bd817c16e3
Prentice Britton <>
2020-09-27 04:09:31 -0500

    Added test code to test functions from Matrix and Level Class.

--- src/main/java/WordMatrix.java
+++ src/main/java/WordMatrix.java
@@ -1,6 +1,13 @@
-
 public class WordMatrix {
- public static void main(String args[]) {
-     
- }   
+
+    public static void main(String args[]) {
+
+        Level testLevel = new Level();
+        Matrix testMatrix = new Matrix(7);
+        String testWord = "chicky";
+        System.out.println(testMatrix);
+        testMatrix.placeWordInMatrix(testWord);
+        System.out.println(testMatrix);
+
+    }
 }
