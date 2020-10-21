8441920820f6c974b76f33822157ad818219999e
Kristin Davis <daviss755@gmail.com>
2020-10-20 22:18:58 -0500

    Refined Level Class

--- src/main/java/Level.java
+++ src/main/java/Level.java
@@ -5,22 +5,14 @@ import java.util.Random;/**
  */
 public class Level {
 	//class variables
-	int level;		//this # is the level's difficulty (or is it unique ID?)
-	int matWidth; 	//this is the x-value for the matrix
-	int matHeight;	//this is the y-value for the matrix
+	int level;              //this # is the level's difficulty (or is it unique ID?)
+	int matWidth;           //this is the x-value for the matrix
+	int matHeight;          //this is the y-value for the matrix
 	int counter = 0;	//this is the counter for the amount of games the player has completed
-				//while not used now, will be used to calculate score later
-	
-	Timer timer = new Timer();	//this is the variable/object for the timer for each level
-	/**
-         * constructor 
-         * initialize the values
-         */
+	String word;		//while not used now, will be used to calculate score later
+
 	public Level() {
-                level = 0;
-		//calls method to determine size
-		determineMatrixSize();
-		
+            level = 0;	
 	}
 	
         /**
@@ -28,15 +20,23 @@ public class Level {
          * @param level current level
          * @return array of integer
          */
-	public int determineMatrixSize () {
-		
-		int starting = 4; //starting matrix size for level 0
-                starting = starting + this.level; // adds the level to the starting array due to it scaling linearly. 
-		matWidth = starting;
-		matHeight = starting;
-		return matWidth;
-                
+	public void determineMatrixSize () {
+            int starting = 4; //starting matrix size for level 0
+            starting = starting + this.level; // adds the level to the starting array due to it scaling linearly. 
+            matWidth = starting;
+            matHeight = starting;
 	}
+        
+        public void createMatrix() {
+            Matrix matrix = new Matrix(matWidth);
+            matrix.generateMatrix();
+            word = matrix.getWord();
+        }
+        // Returns word in matrix 
+        public String retrieveWord() {
+            return word;
+        }
+        
         /**
          * increase level by one
          * @return current level
@@ -46,17 +46,8 @@ public class Level {
             level++;
         }
         
+        // Returns current level
         public int getLevel(){
             return level;
-        }
-
-        /**
-         * Game Duration play
-         * @return duration time in seconds
-         */
-        public int determineTimerDuration() {
-            int totalDuration=30;   //Default duration of the Game
-            int durationLevel = 5; // Duration level 5 seconds
-            return totalDuration - (level*durationLevel);
         }
 }
