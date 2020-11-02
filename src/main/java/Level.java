e419f4213d593b4d850da459ba79d509cae9909a
jpalusum <jayasurya931@github.com>
2020-11-02 15:17:26 -0600

    Done updating the classes and removed Game Screen

--- src/main/java/Level.java
+++ src/main/java/Level.java
@@ -1,81 +1,79 @@
-
-package com.groupg.wordmatrix;
-
 import java.util.Timer;
-
-/**
- *
+import java.util.Random;/**
+ * 
  * @author jayas
  */
 public class Level {
-     //class variables
-
-    int level;		//this # is the level's difficulty (or is it unique ID?)
-    int matWidth; 	//this is the x-value for the matrix
-    int matHeight;	//this is the y-value for the matrix
-    int counter = 0;	//this is the counter for the amount of games the player has completed
-    //while not used now, will be used to calculate score later
-    int highestScore = 0;
-    Timer timer = new Timer();	//this is the variable/object for the timer for each level
-
-    /**
-     * constructor initialize the values
-     */
-    public Level() {
-        level = 0;
-        //calls method to determine size
-        determineMatrixSize();
-
-    }
-
-    /**
-     * matrix allocation size based on level
-     *
-     * @param level current level
-     * @return array of integer
-     */
-    public int determineMatrixSize() {
-
-        int starting = 4; //starting matrix size for level 0
-        starting = starting + this.level; // adds the level to the starting array due to it scaling linearly. 
-        matWidth = starting;
-        matHeight = starting;
-        return matWidth;
-
-    }
-
-    /**
-     * increase level by one
-     *
-     * @return current level
-     */
-    public void increaseLevel() {
-        //Level Increased
-        level++;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    /**
-     * Game Duration play
-     *
-     * @return duration time in seconds
-     */
-    public int determineTimerDuration() {
-        int totalDuration = 30;   //Default duration of the Game
-        int durationLevel = 5; // Duration level 5 seconds
-        return totalDuration - (level * durationLevel);
-    }
-
-    /**
-     * determineHighScore method will assign a Game Highest Score
-     */
-    public void determineHighScore() {
-        if (highestScore < level) {
-            highestScore = level;
+	//class variables
+	int level;              //this # is the level's difficulty (or is it unique ID?)
+	int matWidth;           //this is the x-value for the matrix
+	int matHeight;          //this is the y-value for the matrix
+	int counter = 0;	//this is the counter for the amount of games the player has completed
+	String word;		//while not used now, will be used to calculate score later
+        String playerName="";
+        int highestLevel=0;
+	public Level() {
+            level = 0;	
+	}
+	//i can hear you
+        /**
+         * matrix allocation size based on level
+         * @param level current level
+         * @return array of integer
+         */
+	public void determineMatrixSize () {
+            int starting = 4; //starting matrix size for level 0
+            starting = starting + this.level; // adds the level to the starting array due to it scaling linearly. 
+            matWidth = starting;
+            matHeight = starting;
+	}
+        
+        public void createMatrix() {
+            Matrix matrix = new Matrix(matWidth);
+            matrix.generateMatrix();
+            word = matrix.getWord();
         }
-    }
-
+        // Returns word in matrix 
+        public String retrieveWord() {
+            return word;
+        }
+        
+        /**
+         * increase level by one
+         * @return current level
+         */
+        public void increaseLevel() {
+            //Level Increased
+            level++;
+        }
+        
+        // Returns current level
+        public int getLevel(){
+            return level;
+        }
+        /**
+         * Assign Player Name
+         * @param name 
+         */
+        public void setPlayerName(String name){
+            this.playerName=name;
+        }
+        /**
+         * Set Highest Level
+         */
+        public void setHighestLevel(){
+            if(highestLevel<level){
+                highestLevel = level;
+            }
+        }
+        /**
+         * Get Highest Level
+         * @return 
+         */
+        public int getHighestLevel(){
+            return this.highestLevel;
+        }
+        public void resetLevel(){
+            level=0;
+        }
 }
--- src/main/java/WordMatrix.java
+++ src/main/java/WordMatrix.java
@@ -1,45 +1,121 @@
-package com.groupg.wordmatrix;
-
-import com.groupg.wordmatrix.Level;
-import com.groupg.wordmatrix.EndPage;
-import com.groupg.wordmatrix.Matrix;
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class WordMatrix {
-
-    public static void main(String args[]) {
-startprogram();
+    static boolean isPlaying = false;
+    static String playerAnswer = ""; // The player's input
+    static String answer = ""; // The hidden word in the matrix
+    static Timer timer;
+    static Level level; 
+    // Executes the program with timer
+   public static void run() {
+       long start = System.currentTimeMillis();
+        long end = start + 30*1000; // 30 seconds * 1000 ms/sec
+        System.out.println("\nTimer has Started");
         
-
+        level = new Level();
+        level.determineMatrixSize();
+        level.createMatrix();
+        
+        // As long as player is playing, new matrices will keep generatating after receiving
+        // player's input
+        while (isPlaying = true && System.currentTimeMillis() < end) {
+            answer = level.retrieveWord();
+            Scanner scr = new Scanner(System.in);
+            System.out.println("answer: " + answer); 
+            System.out.println("Enter the word");
+            playerAnswer = scr.nextLine();
+        
+            // If player's input is correct, a larger matrix will generate
+            if (playerAnswer.equals(answer)) {
+                System.out.println();
+                System.out.println("*------------------------------------------------------------------------------------*");
+                System.out.println();
+                System.out.println("Correct! Next Level.");
+                System.out.println();
+                System.out.println("*------------------------------------------------------------------------------------*");
+                System.out.println();
+                level.increaseLevel();
+                level.determineMatrixSize();
+                level.createMatrix();
+               
+            }
+            // If player's input is incorrect, a new matrix of same size will generate
+            else {
+                System.out.println();
+                System.out.println("*------------------------------------------------------------------------------------*");
+                System.out.println();
+                System.out.println("Incorrect. Try Again with A New Matrix.");
+                System.out.println();
+                System.out.println("*------------------------------------------------------------------------------------*");
+                System.out.println();
+                level.createMatrix();
+            }
+        }
+        System.out.println("\nTime's Up!");
+        level.setHighestLevel();
+       String[] args={};
+        main(args);
+        /* ADD END "PAGE" HERE*/
+            
     }
-    public static void startprogram(){
-        Level testLevel = new Level();
-        EndPage remaind;
-        remaind =new EndPage(Integer.valueOf(testLevel.determineTimerDuration()),testLevel);
-        String testWord = "test";
-        System.out.println("Level Class: determineMatrixSize, made to determine the length of the matrix based on the level");
-        System.out.println(testLevel.determineMatrixSize());
-        System.out.println("Level Class: increaseLevel, incremenets the level by one");
-        System.out.println(testLevel.getLevel());
-        testLevel.increaseLevel();
-        remaind =new EndPage(Integer.valueOf(testLevel.determineTimerDuration()),testLevel);
-        System.out.println(testLevel.getLevel());
-        System.out.println("Level Class: determineTimerDuration, made for the timer that will eventually be added to determine the time. Gets smaller as the level increases");
-        System.out.println(testLevel.determineTimerDuration());
+    
+    public static void main(String args[]) {
+        
+        
+        System.out.println("----------------------------------- WORD MATRIX -----------------------------------");
+        System.out.println();
+        
+        System.out.println("Word Matrix is a fun and stimulating game that generates a matrix of\n"
+                + "random characters and a pattern hidden with the matrix. It's your job to find\n"
+                + "the pattern and determine its orientation (whether it occurs vertically or\n"
+                + "horizontally) in the least amount of time possible. Upon each increasing level,\n"
+                + "the matrix increases in size making the pattern more and more difficult to find.");
+        System.out.println();
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("                                    How to Play:                                    ");
+        System.out.println("\n1. Find the hidden word within the matrix");
+        System.out.println("2. Enter the word.");
+        System.out.println("3. Complete as many matrices as you can in 30 seconds.");
+        System.out.println();
+        System.out.println("If your input is correct, you'll advance to the next level "
+                + "and the matrix will increase in size.\nIf it's incorrect, a new matrix of the same"
+                + "size will continue generate until you've successfully entered the correct information.");
+        if(level!=null){
+             System.out.println("Highest Level: "+level.getHighestLevel());
+        }else{
+            System.out.println("Highest Level: 0");
+        }
+       
+        System.out.println();
+        System.out.println("                            Got it? Enter \"Y\" to Play!                            ");
+        System.out.println("------------------------------------------------------------------------------------");
+        Scanner scnr = new Scanner(System.in);
+        char input = scnr.next().charAt(0);
+        
+        // If input equals "N" or "n", the program will stop executing
+        if (input == 'N' || input == 'n') {
+            isPlaying = false;
+           System.out.println("Goodbye!");
+                System.exit(0); 
+        }
+        // If input is invalid, program will continue to ask for correct input
+        else while (input != 'Y' && input != 'y') {
+            System.out.println("Invalid Option. Enter \"Y\" to play, else enter \"N\" to exit.");
+            input = scnr.next().charAt(0);
+            if (input == 'N' || input == 'n') {
+                isPlaying = false;
+                System.out.println("Gone so soon? Goodbye!");
+                System.exit(0);
+            }
+        } 
+        // If input is "Y" or "y", the game will start
+        if (input == 'Y' || input == 'y') {
+           isPlaying = true;
+           run();
+          
+        }
 
-        System.out.println("Matrix Class: generateMatrix, called when constructed and can be called seperatly to make a new matrix");
-        Matrix testMatrix = new Matrix(testLevel.determineMatrixSize());
-        System.out.println("Matrix Class: toString");
-        System.out.println(testMatrix);
-        System.out.println("Matrix Class: determineMatrixSize, determines the maximum length of the word that can be placed in the matrix");
-        System.out.println(testMatrix.determineWordLength());
-        System.out.println("Matrix Class: placeWordInMatrix, randomly places a word in the matrix with random orientation and stores the word along with its position. The test word is test in lowercase");
-        testMatrix.placeWordInMatrix("test");
-        System.out.println(testMatrix);
-    }
-
-    private static EndPage Remainder(Integer valueOf) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
